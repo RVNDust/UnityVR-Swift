8c4e7fa7ddc14f03ab21bba532e3ff47897bd442
Unknown <yvan.denais@viacesi.fr>
2019-01-15 23:25:55 +0100

    FP2-6 - Power management

---
+++ Assets/Swift/Scripts/MachineBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineBehaviour : MonoBehaviour {
+
+    GameObject lightbulb;
+    public Material highlight;
+    public Material unhighlight;
+
+    void Start()
+    {
+        lightbulb = transform.Find("Lightbulb").gameObject;
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Electricity"))
+        {
+            lightbulb.GetComponent<Renderer>().material = highlight;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Electricity"))
+        {
+            lightbulb.GetComponent<Renderer>().material = unhighlight;
+        }
+    }
+}
