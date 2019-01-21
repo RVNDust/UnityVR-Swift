3bfec683be72c82db11f3f91c11a743df8cb5c2d
ExiaA5-RARV <yvan.denais@viacesi.fr>
2019-01-21 17:29:40 +0100

    UI Radar

---
+++ Assets/Swift/Scripts/Tools/ToolRadar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace Swift
+{
+    public class ToolRadar : ToolBehaviour {
+
+        public GameObject RadarWindowPrefab;
+
+        void Start()
+        {
+            StartBehaviour();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            TriggerEnterBehaviour(other);
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            TriggerExitBehaviour(other);
+        }
+
+        public override void ActivateTool(GameObject goRef)
+        {
+            StartBehaviour();
+            //SaveLoadCanvasBehaviour window = vrPlayer.GetComponent<WindowsManager>().CreateWindow(SaveLoadWindowPrefab).GetComponent<SaveLoadCanvasBehaviour>(); //Délègue la création de la fenêtre
+            //goRef.AddComponent<ToolScreenshot>().screenshotManager = cam.GetComponent<ScreenshotManager>(); //Ajout du comportement spécifique sur le controller
+        }
+
+        public override void DesactivateTool(GameObject goRef)
+        {
+            Destroy(goRef.GetComponent<ToolScreenshot>());
+        }
+    }
+}
+
---
+++ Assets/Swift/Scripts/UI/RadarCanvasBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Swift
+{
+    public class RadarCanvasBehaviour : CanvasBehaviour
+    {
+        GameObject localPlayer;
+        List<GameObject> playerOthers = new List<GameObject>();
+
+        public GameObject UserOtherPrefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            StartBehaviour();
+        }
+
+        // Update is called once per frame
+        void Awake()
+        {
+            AwakeBehaviour();
+            GameObject[] playersEntities = GameObject.FindGameObjectsWithTag("Player");
+            foreach (var player in playersEntities)
+            {
+                if (player.GetComponent<NetworkIdentity>().isLocalPlayer)
+                {
+                    localPlayer = player;
+                }
+                else
+                {
+                    playerOthers.Add(player);
+                }
+            }
+        }
+    }
+}
+
